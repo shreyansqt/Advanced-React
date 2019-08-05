a0e7b4922164d03f14e331e4957e39d97fdf7236
Shreyans Jain <shreyans@Shreyanss-MBP.fritz.box>
2019-08-05 23:32:49 +0200

    Fast forward to video 8

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,20 +1,24 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Link from "next/link";
+import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 
-const Nav = props => {
-  return (
-    <div>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="/sell">
-        <a>Sell</a>
-      </Link>
-    </div>
-  );
-};
-
-Nav.propTypes = {};
+const Nav = () => (
+  <NavStyles>
+    <Link href="/items">
+      <a>Items</a>
+    </Link>
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+    <Link href="/signup">
+      <a>Signup</a>
+    </Link>
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="/me">
+      <a>Account</a>
+    </Link>
+  </NavStyles>
+);
 
 export default Nav;
