cd704f5d1123f5de1a969a431397b64b47ff22ca
Shreyans Jain <shreyans@Shreyanss-MacBook-Pro.local>
2019-08-06 16:02:57 +0200

    add Item model/type/query/mutations

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,11 +1,14 @@
 const mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    const newDog = {
-      name: args.name
-    };
-    global.dogs.push(newDog);
-    return newDog;
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if the user is logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: { ...args }
+      },
+      info
+    );
+
+    return item;
   }
 };
 
